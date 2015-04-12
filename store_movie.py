e436a73a2deb512e926dced0ade973e22980f933
Brian Schuster <brian.daniel.schuster@gmail.com>
2015-04-12 12:24:31 -0400

    Changing Server

--- store_movie.py
+++ store_movie.py
@@ -1,5 +1,21 @@
 import cgi
 
+print "Content-type: text/html"
+
+print """
+<html>
+
+<head><title>Printing Movie Name</title></head>
+
+<body>
+
+	<h3>Movie Name Entered:</h3>
+"""
+
 form = cgi.FieldStorage()
-movie_name = form.getvalue('movie_name')
-print movie_name
+movie_name = form.getvalue("Movie Name", 'movie_name')
+
+print """
+</body>
+</html>
+"""
